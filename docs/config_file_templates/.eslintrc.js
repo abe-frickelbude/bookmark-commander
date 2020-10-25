b9fd71310c1eb37c214fe46e7784a71cb00af94e
Aetsch Developers <aetsch.dev@gmail.com>
2020-10-25 13:59:45 +0100

    added config file templates

---
+++ docs/config_file_templates/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', 150],
+    'no-trailing-spaces': 'off',
+    'padded-blocks': 'off',
+    'lines-between-class-members': 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ docs/config_file_templates/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+  printWidth: 200,
+  tabWidth: 2,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'es5',
+};
